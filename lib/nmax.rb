a6678f1f169af51ff95011859e886f2d2ffe6332
kanigreg <kanigreg@gmail.com>
2022-11-08 18:53:16 +0500

    First implementation

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -4,5 +4,27 @@ require_relative 'nmax/version'
 
 module Nmax
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def max_numbers(count, stream = $stdin)
+      @buffer ||= Array.new(count)
+
+      while (readed = stream.gets)
+        number = Integer(readed)
+        handle(number)
+      end
+
+      @buffer.compact
+    end
+
+    private
+
+    def handle(number)
+      index = @buffer.bsearch_index { |n| n.nil? || n < number }
+
+      return if index.nil?
+
+      @buffer.pop
+      @buffer.insert(index, number)
+    end
+  end
 end
--- test/test_nmax.rb
+++ test/test_nmax.rb
@@ -6,4 +6,11 @@ class TestNmax < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Nmax::VERSION
   end
+
+  def test_should_find_n_max
+    file = File.open('test/fixtures/data.txt', mode: 'r')
+    expected = [1000, 727]
+
+    assert_equal expected, Nmax.max_numbers(2, file)
+  end
 end
